98048b78c8e5e9b7a596dd2d7b66fc7c6cc07e8d
Mohammed <mohammed.s.dev@gmail.com>
2021-02-16 23:41:53 +0200

    Relationship Created Between Product & Reviews

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Review;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Product extends Model
 {
-    use HasFactory;
+
+    /**
+    * Get all reviews for product.
+    * @version 1.0 
+    * @author Mohammed M.Salha
+    */
+    public function reviews(){
+        return $this->hasMany(Review::class);
+    }
 }
--- app/Models/Review.php
+++ app/Models/Review.php
@@ -1,11 +1,21 @@
 <?php
 
 namespace App\Models;
-
+use App\Models\Product; 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+
+
 class Review extends Model
 {
-    use HasFactory;
+
+    /**
+    * Get the product for review.
+    * @version 1.0 
+    * @author Mohammed M.Salha
+    */
+    public function product(){
+        return $this->belongsTo(Product::class);
+    }
 }
