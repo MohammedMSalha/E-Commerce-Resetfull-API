c0156e5f27476e23ebc9bb9a820cc757db4a229d
Mohammed <mohammed.s.dev@gmail.com>
2021-02-17 05:02:19 +0200

    modified product details

--- app/Http/Resources/Product/ProductResource.php
+++ app/Http/Resources/Product/ProductResource.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Http\Resources\Product;
-
+use App\Models\Product;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class ProductResource extends JsonResource
@@ -18,8 +18,10 @@ class ProductResource extends JsonResource
             'name'=>$this->name,
             'description'=>$this->details,
             'price'=>$this->price,
-            'stock'=>$this->stock,
-            'discount'=>$this->discount
+            'stock'=>$this->stock==0? 'Out Of Stock!!':$this->stock,
+            'rating'=>$this->reviews->count()>0 ?round($this->reviews->sum('star')/$this->reviews->count(),2):'No Rating Yet !!!',
+            'discount'=>$this->discount,
+            'href'=>[ 'reviews'=>route('reviews.index',$this->id) ]
         ];
     }
 }
