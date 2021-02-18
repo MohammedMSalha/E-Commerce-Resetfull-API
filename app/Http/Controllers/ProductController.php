8c6df8003b72c0caed53634856dac9d3f433382d
Mohammed <mohammed.s.dev@gmail.com>
2021-02-18 21:54:28 +0200

    Product Created

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -6,9 +6,17 @@ use App\Models\Product;
 use Illuminate\Http\Request;
 use App\Http\Resources\Product\ProductResource;
 use App\Http\Resources\Product\ProductCollection;
-
+use App\Http\Requests\ProdcutRequest;
+use Symfony\Component\HttpFoundation\Response;
 class ProductController extends Controller
 {
+
+
+
+    public function __construct()
+    {
+        $this->middleware('auth:api')->except('index','show');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -27,9 +35,18 @@ class ProductController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(ProdcutRequest $request)
     {
-        //
+        $product=new Product;
+        $product->name=$request->name;
+        $product->details=$request->description;
+        $product->price=$request->price;
+        $product->stock=$request->stock;
+        $product->discount=$request->discount;
+        $product->save();
+        return response(
+            ['data'=>new ProductResource($product)],Response::HTTP_CREATED 
+        );
     }
 
     /**
